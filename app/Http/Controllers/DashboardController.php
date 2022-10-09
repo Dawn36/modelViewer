02fe5ea28222bb71a824dd0483114ca3bce92747
Dawn36 <dawngill08@gmail.com>
2022-10-10 03:26:45 +0500

    done

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\TheModel;
+use Illuminate\Support\Facades\Artisan;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        // Artisan::call('config:cache');
+        // Artisan::call('cache:clear');
         $userId=Auth::user()->id;
         $themodelcount=TheModel::where('user_id',$userId)->count();
         return view('dashboard',compact('themodelcount'));
